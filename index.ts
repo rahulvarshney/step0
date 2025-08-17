51187db7b6c3c96e3ddacb86949b822bb081316f
rahulvarshney <rahulvarshney@gmail.com>
2025-08-17 13:30:32 -0700

    0.0.3

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 function handler(_req: Request): Promise<Response> {
     const postRequest = new Request("https://api.forwardemail.net/v1/emails", {
         method: "POST",
-        body: "from=support%40740bsecure.com&to=669bluejay%40gmail.com&subject=AJDLclub&text=party",
+        body: "from=support%40740bsecure.com&to=669bluejay%40gmail.com&subject=AJDLclub&text=party"
         headers: {
             "Content-Type": "x-www-form-urlencoded",
-            "Authorization": 'Basic OWRhOTZjZmI5OGU1ZWIxOWMwYjA0ODUxOg=='
+            "Authorization": "Basic OWRhOTZjZmI5OGU1ZWIxOWMwYjA0ODUxOg=="
 
         },
     });
