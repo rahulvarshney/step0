72315b4ee03c60157dbf34f76ccbbd3c01b8e9ae
rahulvarshney <rahulvarshney@gmail.com>
2025-08-17 13:46:41 -0700

    application

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 function handler(_req: Request): Promise<Response> {
     const postRequest = new Request("https://api.forwardemail.net/v1/emails", {
         method: "POST",
-        body: "from=support%40740bsecure.com&to=669bluejay%40gmail.com&subject=AJDLclub&text=party"
+//        method: "GET",
         headers: {
-            "Content-Type": "x-www-form-urlencoded",
+            "Content-Type": "application/x-www-form-urlencoded",
             "Authorization": "Basic OWRhOTZjZmI5OGU1ZWIxOWMwYjA0ODUxOg=="
 
         },
+        body: "?from=support%40740bsecure.com&to=669bluejay%40gmail.com&subject=AJDLclub&text=party",
     });
 
     // Make the POST request and return its response
